b184c9ca42ec5c2fbc28b9d9ad0fbc160b333a1e
Your Name <=>
2021-08-17 22:19:05 -0500

    adds miminal CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,10 +11,10 @@ const IndexPage = () => {
       <Head page="Home" />
       <div class = "container" style={{ display:"flex", margin:"auto" }}>
       
-    <div class="col-s6" style={{display:"flex", margin:"10px"}}><img src="images/image-four.png" id="img" class="rounded-circle img-fluid border border-danger"  />  </div>
+    <div class="col-s6" style={{display:"flex", margin:"10px"}}><img src="images/image-four.png" id="img" class="rounded-circle img-fluid border border-primary"  />  </div>
     
 <div class="circular-sb col-s6">
-<h1>An aspiring web developer who is eager to learn and to keep improving, working while travelling is definetly a dream that I am working hard on achieving !!</h1>  <div class="circle3"></div>
+<h3>An aspiring web developer who is eager to learn and to keep improving, working while travelling is definetly a dream that I am working hard on achieving !!</h3>  <div class="circle3"></div>
   <div class="circle4"></div>
 </div>
 
